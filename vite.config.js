a1abb5c2496c47b63783a8942f60d777eabc47be
Allen Yeh <allen_yeh@systemweb.com.tw>
2022-12-17 03:57:06 +0800

    ✨feature: svg-sprite-loader

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { URL, fileURLToPath } from 'node:url'
-
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import { svgBuilder } from './src/plugins/svgBuilder'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), svgBuilder('./src/assets/icons/')],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
