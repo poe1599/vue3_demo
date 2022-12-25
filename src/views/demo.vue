9cc92afa7241104f85b74256d35f1cc3fb5173f3
Allen Yeh <allen_yeh@systemweb.com.tw>
2022-12-26 00:57:29 +0800

    ✨feature: svg / logo / favicon

--- src/views/demo.vue
+++ src/views/demo.vue
@@ -5,6 +5,35 @@
       <h2>Button</h2>
       <p><button class="hollowButton">Hollow Button</button> <button class="solidButton">Solid Button</button></p>
     </div>
+
+    <div>
+      <h2>Svg</h2>
+      <p>
+        <SvgIcon class="attractions" name="zeelandia"></SvgIcon>
+        <SvgIcon class="attractions" name="bicycle"></SvgIcon>
+        <SvgIcon class="attractions" name="lamp"></SvgIcon>
+        <SvgIcon class="attractions" name="museum"></SvgIcon>
+        <SvgIcon class="attractions" name="queen"></SvgIcon>
+        <SvgIcon class="attractions" name="bubbleTea"></SvgIcon>
+        <SvgIcon class="attractions" name="ballon"></SvgIcon>
+        <SvgIcon class="attractions" name="hotel"></SvgIcon>
+      </p>
+      <p>
+        <SvgIcon class="icon" name="boldArrow"></SvgIcon>
+        <SvgIcon class="icon" name="search"></SvgIcon>
+        <SvgIcon class="icon" name="time"></SvgIcon>
+        <SvgIcon class="icon" name="printer"></SvgIcon>
+        <SvgIcon class="icon" name="call"></SvgIcon>
+        <SvgIcon class="icon" name="share"></SvgIcon>
+        <SvgIcon class="icon" name="location"></SvgIcon>
+        <SvgIcon class="icon" name="chevron"></SvgIcon>
+        <SvgIcon class="icon" name="sort"></SvgIcon>
+        <SvgIcon class="icon" name="setting"></SvgIcon>
+      </p>
+      <p>
+        <img src="@/assets/images/logo.svg" alt="logo">
+      </p>
+    </div>
   </div>
 </template>
 <script>
@@ -12,4 +41,16 @@ export default {
   name: 'DemoIndex',
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.attractions {
+  width: 64px;
+  height: 64px;
+  color: $primary-1;
+}
+
+.icon {
+  width: 24px;
+  height: 24px;
+  color: $primary-1;
+}
+</style>
