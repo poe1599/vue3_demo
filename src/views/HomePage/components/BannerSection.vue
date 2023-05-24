7832af125ad6cc7e9ef974067b9bdaa214df368b
Allen Yeh <allen_yeh@systemweb.com.tw>
2023-05-24 19:50:03 +0800

    🆙update: BannerSection

--- src/views/HomePage/components/BannerSection.vue
+++ src/views/HomePage/components/BannerSection.vue
@@ -3,11 +3,19 @@
     <swiper
       :pagination="{ clickable: true }"
       :navigation="true"
-      :modules="[Pagination, Navigation]"
+      :modules="[Autoplay, Pagination, Navigation]"
+      :autoplay="{
+        delay: 5000,
+        disableOnInteraction: false,
+      }"
+      :loop="true"
       class="S-banner__swiper"
     >
-      <swiper-slide v-for="i in 10" :key="i">
-        <div class="S-banner__slide"></div>
+      <swiper-slide v-for="i in BannerList" :key="i.ScenicSpotID">
+        <div class="S-banner__slide">
+          <img class="S-banner__img" :src="i.Picture.PictureUrl1" :alt="i.Picture.PictureDescription1" />
+          <figcaption class="S-banner__fig">{{ i.ScenicSpotName }}</figcaption>
+        </div>
       </swiper-slide>
     </swiper>
   </section>
@@ -18,11 +26,34 @@ export default {
 }
 </script>
 <script setup>
-import { Navigation, Pagination } from 'swiper'
+import { Autoplay, Navigation, Pagination } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/vue'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
+import '@/css/custom-swiper.css'
+import { onMounted, reactive } from 'vue'
+import { getScenicSpot } from '@/api/request/tourism'
+import getRandom from '@/plugins/getRandom'
+import mockData from '@/views/HomePage/components/mockBanner'
+
+const BannerList = reactive([])
+
+const getScenicSpotBannerData = async () => {
+  const $skip = getRandom(0, 500)
+  const response = await getScenicSpot({ params: { $skip, $top: 30 } })
+  if (!response) return
+
+  const list = response.filter(i => i.Picture.PictureUrl1).filter((i, iIdx) => iIdx < 10)
+  BannerList.push(...list)
+
+  // // mock
+  // BannerList.push(...mockData)
+}
+
+onMounted(async () => {
+  await getScenicSpotBannerData()
+})
 </script>
 <style lang="scss" scoped>
 .S-banner {
@@ -37,12 +68,40 @@ import 'swiper/css/pagination'
   }
 
   &__slide {
+    position: relative;
     overflow: hidden;
     width: 100%;
     height: 100%;
     background: $fake-bg-color;
     border: 1px solid $fake-bg-color;
     border-radius: 1rem;
+  }
+
+  &__img {
+    position: absolute;
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    cursor: pointer;
+  }
+
+  &__fig {
+    position: absolute;
+    bottom: 1rem;
+    left: 1rem;
+    padding: 0.5rem;
+    font-size: 1rem;
+    color: var(--surface-a);
+    background-color: var(--surface-800);
+    border-radius: 0.25rem;
+    opacity: 0.7;
+    @include tablet {
+      font-size: 1.5rem;
+    }
+    @include crosswise {
+      font-size: 2rem;
+    }
   }
 }
 </style>
